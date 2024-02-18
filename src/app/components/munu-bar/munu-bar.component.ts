b4b5b298ea49b34d22c5eec5448d78b46e3c7eaa
PauloHM65 <pauloharaujo345@gmail.com>
2024-02-17 22:50:18 -0300

    components with styles

---
+++ src/app/components/munu-bar/munu-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-munu-bar',
+  templateUrl: './munu-bar.component.html',
+  styleUrls: ['./munu-bar.component.css']
+})
+export class MunuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/munu-title/munu-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-munu-title',
+  templateUrl: './munu-title.component.html',
+  styleUrls: ['./munu-title.component.css']
+})
+export class MunuTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
