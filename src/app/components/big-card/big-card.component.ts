f2729d4cfdb1d27326fb02d2f02f887d92668775
PauloHM65 <pauloharaujo345@gmail.com>
2024-02-17 23:32:41 -0300

    dynamics components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  
+  photoCover:string = "https://designconceitual.com.br/wp-content/uploads/2023/12/Angular-novo-logotipo-2023-1000x600.jpg"
+
+  @Input()
+  cardTitle:string = ""
+
+  cardDescription:string = "A equipe do Angular não está medindo esforços para trazer grandes novidades a cada versão lançada. Foi assim nas versões 14, 15 e 16. Agora com a versão 17 não é diferente. O Angular progrediu muito em pouquíssimo tempo."
+  
 
   constructor() { }
 
